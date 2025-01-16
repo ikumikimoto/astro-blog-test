e6e84fa414d5ba1bb72396385807c481a8bbe609
Ikumi Kimoto <kimoto@hakoreco.com>
2025-01-16 21:52:13 +0900

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://ikumikimoto.github.io',
+	base: '/astro-blog-test',
 	integrations: [mdx(), sitemap()],
 });
